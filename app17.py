9d1927f3c7af3fc1ab7559f84e3359785831faf1
Martin Bernard <119161281+martin-bernard26@users.noreply.github.com>
2024-11-26 22:24:38 +0700

    Update app17.py

--- app17.py
+++ app17.py
@@ -124,3 +124,238 @@ with tab[1]:
     nama = st.text_input("Nama",key="Nama")
     kelas = st.text_input("kelas",key="kelas")
     sekolah = st.text_input("sekolah",key="sekolah")
+    if nama and kelas and sekolah:
+        st.markdown('''
+                <div><b>Petunjuk</b>
+                <ol type="a">
+                    <li>Selesaikan tugas menyenangkan berikut!</li>
+                    <li>Tulis jawaban dengan benar dan jelas!</li>
+                    <li>Soal-soal berikut berdasarkan teks bacaan yang berjudul “Hemat Air, Cara Mudah Menyelamatkan Bumi”.</li>
+                </ol>
+                </div>
+                ''', unsafe_allow_html=True)
+        tulisan_html3=f'''
+        <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .pertanyaan{{
+            font-family:"comic sans ms";
+            font-size:20px;
+            margin:10px;
+            color:blue;
+        }}
+        .pertanyaan1, .pertanyaan2{{
+            font-size:18px;
+            color:black;
+            font-weight:bold;
+            margin:5px;
+        }}
+        .pertanyaan3{{
+            border:2px solid brown;
+            width:300px;
+            height:30px;
+            background-color:white;
+            border-radius:10px;
+            font-size:15px;
+            padding:3px;
+        }}
+        .pertanyaan3:hover{{
+            background-color:yellow;
+            border-radius:5px;
+        }}
+        .pertanyaan4, .pertanyaan5{{
+            border-radius:10px;
+            border:2px solid black;
+            box-shadow:2px 2px 2px 2px green;
+            background-color:white;
+            font-size:15px;
+            padding:3px;
+        }}
+        .pertanyaan4:hover, .pertanyaan5:hover{{
+            background-color:cyan;
+        }}
+        .pertanyaan4:focus, .pertanyaan5:focus{{
+            background-color:lightgoldenrodyellow;
+            font-size:15px;
+            padding:3px;
+        }}
+        #kirim1{{
+            font-family:nroadway;
+            font-size:20px;
+            background-color:green;
+            color:yellow;
+        }}
+    </style>
+</head>
+<body>
+    <div>
+        <div><input type="text" id="nama" value={nama}></div>
+        <div><input type="text" id="kelas" value={kelas}></div>
+        <div><input type="text" id="ns" value={sekolah}></div>
+    </div>
+    <div>
+        <ol>
+            <li class="pertanyaan"><b>Artikan kata-kata/kelompok kata berikutnya</b>
+                <ol type="a">
+                    <li class="pertanyaan1">Konsumsi  (baris ke-6)  =  <input class="pertanyaan3" type="text"> </li>
+                    <li class="pertanyaan1">Rekomendasi (baris ke-10)  = <input class="pertanyaan3" type="text"> </li>
+                    <li class="pertanyaan1">Idealnya (baris ke-11) =  <input class="pertanyaan3" type="text"> </li>
+                    <li class="pertanyaan1">Membentengi diri (baris ke-12)  =   <input class="pertanyaan3" type="text"> </li>
+                    <li class="pertanyaan1">Merekomendasikan (baris ke-25)  = <input class="pertanyaan3" type="text"></li>
+                </ol>
+            </li>
+            <li class="pertanyaan"><b>Jawablah pertenyaan di bahwah ini</b>
+                <ol type="a">
+                    <li class="pertanyaan2"><div>Apa nama masakan yang mengandung sayur, yang ada dalam teks bacaan tadi?</div>
+                        <div><textarea class="pertanyaan5" cols="40" rows="5"></textarea></div>
+                     </li>
+                    <li class="pertanyaan2"><div>Apa saja zat penting yang terkandung dalam sayur dan buah?</div>
+                        <div><textarea class="pertanyaan5" cols="40" rows="5"></textarea></div>
+                     </li>
+                    <li class="pertanyaan2"><div>Apa yang dibahas dalam teks bacaan tadi?</div>
+                        <div><textarea class="pertanyaan5" cols="40" rows="5"></textarea></div>
+                    </li>
+                </ol>
+            </li>
+            <li class="pertanyaan">
+                <b>Tuliskan informasi-informasi penting dari teks bacaan tadi!</b>
+                <div><textarea class="pertanyaan4" cols="40" rows="10"></textarea></div>
+            </li>
+            <li class="pertanyaan">
+                <b>Tuliskan informasi-informasi penting dari teks bacaan tadi!</b>
+                <div><textarea class="pertanyaan4" cols="40" rows="10"></textarea></div>
+            </li>
+            <li class="pertanyaan">
+                <b>Uraikan ide pokok dan ide penjelas dalam teks bacaan tadi!</b>
+                <div><textarea class="pertanyaan4" cols="40" rows="10"></textarea></div>
+            </li>
+            <li class="pertanyaan">
+                <b>Urutkan kejadian-kejadian dari teks bacaan tadi!</b>
+                <div><textarea class="pertanyaan4" cols="40" rows="10"></textarea></div>
+            </li>
+            <li class="pertanyaan">
+                <b>Tentukan kalimat utama dalam teks bacaan tadi!</b>
+                <div><textarea class="pertanyaan4" cols="40" rows="10"></textarea></div>
+            </li>
+            <li class="pertanyaan">
+                <b>Urutkan fakta-fakta/informasi penting dari teks bacaan tadi!</b>
+                <div><textarea class="pertanyaan4" cols="40" rows="10"></textarea></div>
+            </li>
+            <li class="pertanyaan">
+                <b>Tuliskan simpulan yang mengakibatkan masyarakat Indonesia kurang
+                mengonsumsi sayur dan buah!</b>
+                <div><textarea class="pertanyaan4" cols="40" rows="10"></textarea></div>
+            </li>
+            <li class="pertanyaan">
+                <b>Ceritakan kembali dengan bahasamu sendiri isi teks bacaan tadi!</b>
+                <div><textarea class="pertanyaan4" cols="40" rows="10"></textarea></div>
+            </li>
+            <li class="pertanyaan">
+                <b>Isi dalam teks bacaan tadi banyak mengandung makna/nilai-nilai/kebiasaan yang bisa kita tiru. 
+                Tuliskan kebiasaan/nilai-nilai yang kamu pahami dari teks bacaan tadi!</b>
+                <div><textarea class="pertanyaan4" cols="40" rows="10"></textarea></div>
+            </li>
+            <li class="pertanyaan">
+                <b>Menurutmu, apakah teks bacaan tadi jelas dan lengkap? Apa alasanmu?</b>
+                <div><textarea class="pertanyaan4" cols="40" rows="10"></textarea></div>
+            </li>
+            <li class="pertanyaan">
+                <b>Menurutmu, alur tulisan yang dituliskan penulis sudah benar? Apa alasanmu?</b>
+                <div><textarea class="pertanyaan4" cols="40" rows="10"></textarea></div>
+            </li>
+            <li class="pertanyaan">
+                <b>Berdasarkan cerita dalam teks tadi, apakah ada informasi, data, atau fakta yang kamu temui
+                dalam kehidupan sehari-hari? Jika ada, coba tuliskan!</b>
+                <div><textarea class="pertanyaan4" cols="40" rows="10"></textarea></div>
+            </li>
+            <li class="pertanyaan">
+                <b>Menurutmu, apakah isi cerita dalam teks tadi sesuai dengan kehidupan sehari-hari? Mengapa?</b>
+                <div><textarea class="pertanyaan4" cols="40" rows="10"></textarea></div>
+            </li>
+            <li class="pertanyaan">
+                <b>M16.	Setelah membaca cerita tadi, apakah ada nilai/kebiasaan yang perlu ditiru dan tidak boleh ditiru
+                olehmu? Tuliskan nilai-nilai/kebiasaan yang perlu ditiru dan tidak perlu ditiru olehmu, yang akan kamu
+                terapkan dalam kehidupan sehari-hari!</b>
+                <div><textarea class="pertanyaan4" cols="40" rows="10"></textarea></div>
+            </li>
+        </ol>
+    </div>
+    <div><input id="kirim1" type="button" value="Kirim Jawaban" ></div>
+    <script type="module">
+            // Konfigurasi Firebase SDK
+            import {{ initializeApp }} from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+            const firebaseConfig = {{
+                    apiKey: "AIzaSyCkgVmk75UTkos2y1Mrc7d3-sxShMfbeJQ",
+                    authDomain: "natural-ethos-423713-e0.firebaseapp.com",
+                    databaseURL: "https://natural-ethos-423713-e0-default-rtdb.firebaseio.com",
+                    projectId: "natural-ethos-423713-e0",
+                    storageBucket: "natural-ethos-423713-e0.firebasestorage.app",
+                    messagingSenderId: "41833960811",
+                    appId: "1:41833960811:web:6218d6ac2f3538c704e82e",
+            }};
+
+            // Inisialisasi Firebase
+            const app = initializeApp(firebaseConfig);
+            import {{getDatabase, set, get, update, remove, ref, child}}
+                from "https://www.gstatic.com/firebasejs/11.0.2/firebase-database.js";
+            const db=getDatabase()
+            var kirim1 = document.getElementById("kirim1")
+            kirim1.addEventListener("click",()=>{{
+                var nama = document.getElementById("nama").value
+                var kelas = document.getElementById("kelas").value
+                var ns = document.getElementById("ns").value
+                var soal1 = document.getElementsByClassName("pertanyaan3")
+                var soal2 = document.getElementsByClassName("pertanyaan5")
+                var soal3 = document.getElementsByClassName("pertanyaan4")
+                set(ref(db, 'pemburuan_hewan/' + nama), {{ 
+                    nama:nama,
+                    kelas:kelas,
+                    ns:ns,
+                }})
+                    .then(() => {{
+                        alert('Data added successfully');
+                    }})
+                    .catch((error) => {{
+                    console.error("Error adding data:", error);
+                }});
+                set(ref(db, 'sayuran/' + nama+'/soal'), {{ 
+                    soal1a:soal1[0].value,
+                    soal1b:soal1[1].value,
+                    soal1c:soal1[2].value,
+                    soal1d:soal1[3].value,
+                    soal1e:soal1[4].value,
+                    soal2a:soal2[0].value,
+                    soal2b:soal2[1].value,
+                    soal2c:soal2[2].value,
+                    soal3:soal3[0].value,
+                    soal4:soal3[1].value,
+                    soal5:soal3[2].value,
+                    soal6:soal3[3].value,
+                    soal7:soal3[4].value,
+                    soal8:soal3[5].value,
+                    soal9:soal3[6].value,
+                    soal10:soal3[7].value,
+                    soal11:soal3[8].value,
+                    soal12:soal3[9].value,
+                    soal13:soal3[10].value,
+                    soal14:soal3[11].value,
+                    soal15:soal3[12].value,
+                    soal15:soal3[16].value
+                }})
+                    .then(() => {{
+                        alert('Data added successfully');
+                    }})
+                    .catch((error) => {{
+                    console.error("Error adding data:", error);
+                }});
+            }});
+
+    </script>
+</body>
+</html>
+'''
+        st.components.v1.html(tulisan_html3,width=1000,height=13000)
