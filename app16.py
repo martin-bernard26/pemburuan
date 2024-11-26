965f33ab59ead8e95c8b02a4cf4b40cee3da1bac
Martin Bernard <119161281+martin-bernard26@users.noreply.github.com>
2024-11-26 22:07:45 +0700

    Update app16.py

--- app16.py
+++ app16.py
@@ -263,7 +263,7 @@ with tab[1]:
             <li class="pertanyaan"><b>Artikan kata-kata/kelompok kata berikutnya</b>
                 <ol type="a">
                     <li class="pertanyaan1">Hutan Lindung (baris 2)  =  <input class="pertanyaan3" type="text"> </li>
-                    <li class="pertanyaan1">Mengendap-endap (baris 3)  = = <input class="pertanyaan3" type="text"> </li>
+                    <li class="pertanyaan1">Mengendap-endap (baris 3)  =  <input class="pertanyaan3" type="text"> </li>
                     <li class="pertanyaan1">Dihantam (baris 9)  = <input class="pertanyaan3" type="text"> </li>
                     <li class="pertanyaan1">Menguliti (baris 14)  =  <input class="pertanyaan3" type="text"> </li>
                     <li class="pertanyaan1">Perburuan (baris 23) = <input class="pertanyaan3" type="text"></li>
